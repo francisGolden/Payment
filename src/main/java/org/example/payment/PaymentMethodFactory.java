c668c9c625b0c7325770ce4a7d4800e6fe3580b5
Francesco Doria <doria5@live.it>
2026-07-01 06:58:11 +0300

    return CryptoWalletPayment instead of GiftCardPayment

--- src/main/java/org/example/payment/PaymentMethodFactory.java
+++ src/main/java/org/example/payment/PaymentMethodFactory.java
@@ -14,6 +14,6 @@ public class PaymentMethodFactory {
     }
 
     public static PaymentMethod createCryptoWalletPayment(String key, double balance){
-        return new GiftCardPayment(key, balance);
+        return new CryptoWalletPayment(key, balance);
     }
 }
